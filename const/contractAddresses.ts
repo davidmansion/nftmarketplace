11cced53492971af2f06a14c3905aba018bd6c9f
davidmansion <93778907+davidmansion@users.noreply.github.com>
2023-11-05 20:38:41 +0000

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -3,7 +3,7 @@
 // 1. Set up the network your smart contracts are deployed to.
 // First, import the chain from the package, then set the NETWORK variable to the chain.
 import { Binance } from "@thirdweb-dev/chains";
-export const NETWORK = Binance;
+export const NETWORK = BNB Smart Chain;
 
 // 2. The address of the marketplace V3 smart contract.
 // Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
